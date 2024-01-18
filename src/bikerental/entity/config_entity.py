9c3bcc89d3a6d85cf73d90128aa6675bd8ddba3f
rohan <rohannair2939@gmail.com>
2024-01-18 11:53:35 +0530

    config updated

--- src/bikerental/entity/config_entity.py
+++ src/bikerental/entity/config_entity.py
@@ -11,17 +11,12 @@ class DataIngestionConfig:
 
 
 
-# @dataclass(frozen=True)
-# class PrepareBaseModelConfig:
-#     root_dir: Path
-#     base_model_path: Path
-#     updated_base_model_path: Path
-#     params_image_size: list
-#     params_learning_rate: float
-#     params_include_top: bool
-#     params_weights: str
-#     params_classes: int
-
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_n_estimators: int
 
 
 # @dataclass(frozen=True)
